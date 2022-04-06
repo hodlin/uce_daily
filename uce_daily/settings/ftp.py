a28952e85fadd230cd154f2d0b46e181eec30957
Dmytro Domin <mr.diomin@gmail.com>
2022-04-06 16:17:45 +0300

    ftp settings added

--- uce_daily/settings/ftp.py
+++ uce_daily/settings/ftp.py
@@ -5,10 +5,18 @@ import os
 # CEG forecast_user
 ceg_ftp_url = os.environ.get("CEG_FTP_URL")
 
-ceg_fc_settings = {
+FORECAST_FTP = {
     'host': ceg_ftp_url.split(':')[0],
     'port': int(ceg_ftp_url.split(':')[-1]),
     'user': os.environ.get("CEG_FTP_FORECAST_USER_NAME"),
     'passwd': os.environ.get("CEG_FTP_FORECAST_USER_PASSWORD"),
     'forecast_dir': os.environ.get("CEG_FTP_FORECAST_USER_FORECAST_DIR"),
+}
+
+METER_DATA_FTP = {
+    'host': ceg_ftp_url.split(':')[0],
+    'port': int(ceg_ftp_url.split(':')[-1]),
+    'user': os.environ.get("CEG_FTP_UTP_DATA_USER_NAME"),
+    'passwd': os.environ.get("CEG_FTP_UTP_DATA_USER_PASSWORD"),
+    'forecast_dir': os.environ.get("CEG_FTP_UTP_DATA_USER_FORECAST_DIR"),
 }
